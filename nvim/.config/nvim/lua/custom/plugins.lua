a68f5ee1b842e0c4d9ed7a5e8a4d3a7dafd5984a
Michael Czyż <mike@c2yz.com>
2024-02-18 14:24:04 +0100

    git config plus zoxide plugin

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -3,17 +3,14 @@ local overrides = require("custom.configs.overrides")
 ---@type NvPluginSpec[]
 local plugins = {
 
-  -- Override plugin definition options
-
   {
     "neovim/nvim-lspconfig",
     config = function()
       require "plugins.configs.lspconfig"
       require "custom.configs.lspconfig"
-    end, -- Override to setup mason-lspconfig
+    end,
   },
 
-  -- override plugin configs
   {
     "williamboman/mason.nvim",
     opts = {
@@ -31,14 +28,6 @@ local plugins = {
   {
     "nvim-tree/nvim-tree.lua",
     opts = overrides.nvimtree,
-  },
-
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.configs.lspconfig"
-    end,
   },
 
   {
@@ -67,7 +56,12 @@ local plugins = {
       return M
     end,
   },
-  -- Install a plugin
+
+  {
+    "nanotee/zoxide.vim",
+    lazy = false,
+  },
+
   {
     "max397574/better-escape.nvim",
     event = "InsertEnter",
@@ -78,17 +72,10 @@ local plugins = {
 
   {
     "stevearc/conform.nvim",
-    --  for users those who want auto-save conform + lazyloading!
-    -- event = "BufWritePre"
     config = function()
       require "custom.configs.conform"
     end,
   },
-  -- To make a plugin not be loaded
-  -- {
-  --   "NvChad/nvim-colorizer.lua",
-  --   enabled = false
-  -- },
 
   -- All NvChad plugins are lazy-loaded by default
   -- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
